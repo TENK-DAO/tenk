8b087ad7bdd137434041f671746f6a5086d4749d
Willem Wyndham <willem@cs.umd.edu>
2021-07-31 16:38:31 -0400

    chore: clean up raffle

--- contract/src/raffle.rs
+++ contract/src/raffle.rs
@@ -1,13 +1,9 @@
 //! A vector implemented on a trie. Unlike standard vector does not support insertion and removal
 //! of an element results in the last element being placed in the empty position.
+use core::convert::TryInto;
 use std::marker::PhantomData;
 
-use core::{
-  convert::TryInto,
-};
-
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-
 use near_sdk::{env, IntoStorageKey};
 
 const ERR_INCONSISTENT_STATE: &[u8] = b"The collection is an inconsistent state. Did previous smart contract execution terminate unexpectedly?";
@@ -18,7 +14,7 @@ fn expect_consistent_state<T>(val: Option<T>) -> T {
 }
 
 pub(crate) fn append_slice(id: &[u8], extra: &[u8]) -> Vec<u8> {
-  [id, extra].concat()
+    [id, extra].concat()
 }
 
 /// An iterable implementation of vector that stores its content on the trie.
@@ -48,26 +44,17 @@ impl Raffle {
     where
         S: IntoStorageKey,
     {
-        Self { len, prefix: prefix.into_storage_key(), el: PhantomData }
+        Self {
+            len,
+            prefix: prefix.into_storage_key(),
+            el: PhantomData,
+        }
     }
 
     fn index_to_lookup_key(&self, index: u64) -> Vec<u8> {
         append_slice(&self.prefix, &index.to_le_bytes()[..])
     }
 
-    /// Returns the serialized element by index or `None` if it is not present.
-    // pub fn get_raw(&self, index: u64) -> Option<Vec<u8>> {
-    //     if index >= self.len {
-    //         return None;
-    //     }
-    //     let lookup_key = self.index_to_lookup_key(index);
-    //     Some(expect_consistent_state(env::storage_read(&lookup_key)))
-    // }
-
-    /// Removes an element from the vector and returns it in serialized form.
-    /// The removed element is replaced by the last element of the vector.
-    /// Does not preserve ordering, but is `O(1)`.
-    ///
     /// # Panics
     ///
     /// Panics if `index` is out of bounds.
@@ -78,54 +65,44 @@ impl Raffle {
             expect_consistent_state(self.pop_raw())
         } else {
             let lookup_key = self.index_to_lookup_key(index);
-            let raw_last_value = self.pop_raw().expect("checked `index < len` above, so `len > 0`");
+            let raw_last_value = self
+                .pop_raw()
+                .expect("checked `index < len` above, so `len > 0`");
             if env::storage_write(&lookup_key, &raw_last_value) {
                 expect_consistent_state(env::storage_get_evicted())
             } else {
+                // no value was at location it's index is the value
                 index.to_le_bytes().to_vec()
             }
         }
     }
-
-    /// Appends a serialized element to the back of the collection.
-    // pub fn push_raw(&mut self, raw_element: &[u8]) {
-    //     let lookup_key = self.index_to_lookup_key(self.len);
-    //     self.len += 1;
-    //     env::storage_write(&lookup_key, raw_element);
-    // }
 
     /// Removes the last element from a vector and returns it without deserializing, or `None` if it is empty.
     fn pop_raw(&mut self) -> Option<Vec<u8>> {
         if self.is_empty() {
             None
         } else {
-            let last_index = self.len - 1;
-            let last_lookup_key = self.index_to_lookup_key(last_index);
-
             self.len -= 1;
+            let last_lookup_key = self.index_to_lookup_key(self.len);
             let raw_last_value = if env::storage_remove(&last_lookup_key) {
+                // if key is en in storage, it's value will be stored as last evicted value
                 expect_consistent_state(env::storage_get_evicted())
             } else {
-                last_index.to_le_bytes().to_vec()
+                // otherwise the value is index of the last element
+                self.len.to_le_bytes().to_vec()
             };
             Some(raw_last_value)
         }
     }
 
-
     pub fn draw(&mut self) -> u64 {
-      let seed = env::random_seed();
-      let mut arr: [u8; 8] = Default::default();
-      arr.copy_from_slice(&seed[..8]);
-      let seed_num: u64 = u64::from_le_bytes(arr).try_into().unwrap();
-      u64::try_from_slice(&self.swap_remove_raw(seed_num % self.len())).unwrap()
-
-
+        let seed = env::random_seed();
+        let mut arr: [u8; 8] = Default::default();
+        arr.copy_from_slice(&seed[..8]);
+        let seed_num: u64 = u64::from_le_bytes(arr).try_into().unwrap();
+        u64::try_from_slice(&self.swap_remove_raw(seed_num % self.len())).unwrap()
     }
-  }
-
-
-
+}
 
 #[cfg(not(target_arch = "wasm32"))]
 #[cfg(test)]
@@ -133,18 +110,16 @@ mod tests {
     use rand::{Rng, SeedableRng};
     use std::collections::HashSet;
 
-
     use super::Raffle;
     use near_sdk::test_utils::test_env;
     use near_sdk::test_utils::VMContextBuilder;
     use near_sdk::testing_env;
 
-
     #[test]
     pub fn test_swap_remove() {
         test_env::setup();
         let mut rng = rand_xorshift::XorShiftRng::seed_from_u64(2);
-        let mut vec = Raffle::new(b"v".to_vec(),100);
+        let mut vec = Raffle::new(b"v".to_vec(), 100);
         let mut set: HashSet<u64> = HashSet::new();
         let mut context = VMContextBuilder::new();
         testing_env!(context.build());
@@ -156,6 +131,4 @@ mod tests {
             assert_eq!(len - 1, vec.len())
         }
     }
-
-
 }
