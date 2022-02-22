167512d1f1a883370849c9403da0afe7dee9a1db
Willem Wyndham <wyndham@cs.unc.edu>
2022-02-22 17:57:31 -0500

    fix: mint only one at a time

--- contracts/tenk/src/lib.rs
+++ contracts/tenk/src/lib.rs
@@ -334,8 +334,7 @@ impl Contract {
         self.nft_mint_many(1)[0].clone()
     }
 
-    #[payable]
-    pub fn nft_mint_many(&mut self, num: u32) -> Vec<Token> {
+    fn nft_mint_many(&mut self, num: u32) -> Vec<Token> {
         let owner_id = &env::signer_account_id();
         let num = self.assert_can_mint(owner_id, num);
         let tokens = self.nft_mint_many_ungaurded(num, owner_id, false);
