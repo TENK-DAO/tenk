eebc6e8a1cb4513b14eac15a96b1786d01bee54d
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2022-04-13 11:40:25 -0400

    build: switch from yarn to npm
    flailing attempt to fix installation of lodash in CI

--- gui/src/index.tsx
+++ gui/src/index.tsx
@@ -1,13 +1,10 @@
 import React from "react";
-import { createRoot } from 'react-dom/client';
+import ReactDOM from "react-dom";
 import { Contract, Home } from "./components"
 import { HashRouter, Routes, Route } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 
-const container = document.getElementById("root")!;
-const root = createRoot(container);
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
         <Routes>
@@ -16,7 +13,8 @@ root.render(
           <Route path="/:contract/:method" element={<Contract />} />
       </Routes>
     </HashRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
