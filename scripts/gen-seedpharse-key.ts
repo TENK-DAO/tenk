f4452defc5a5322773b49bd1d48f25160e9591fc
Willem Wyndham <wyndham@cs.unc.edu>
2022-05-26 14:13:03 -0400

    feat: add gen-account-from-seedphrase script

--- scripts/gen-seedpharse-key.ts
+++ scripts/gen-seedpharse-key.ts
@@ -1,19 +1,19 @@
-import {Context} from "near-cli/context";
+import { Context } from "near-cli/context";
 import { generateSeedPhrase } from "near-seed-phrase";
 
-export async function main({account, argv}: Context): Promise<void> {
-  if (!account) {
-    throw new Error("Must pass account!")
-  }
-  let keys = generateSeedPhrase();
-  let [add] = argv;
-  if (add === "--help") {
-    console.error(".. --add  Add generated key to account")
-    return;
-  }
-  console.log(keys);
-  if (add === "--add") {
-    await account.addKey(keys.publicKey);
-    console.log("Added key", keys.publicKey);
-  }
+export async function main({ account, argv }: Context): Promise<void> {
+    let keys = generateSeedPhrase();
+    let [add] = argv ?? [undefined];
+    if (add === "--help") {
+        console.error(".. --add  Add generated key to account")
+        return;
+    }
+    console.log(keys);
+    if (add === "--add") {
+        if (!account) {
+            throw new Error("Must pass account if using `--add`")
+        }
+        await account.addKey(keys.publicKey);
+        console.log("Added key", keys.publicKey);
+    }
 }
