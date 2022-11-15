9b99790847df9c7e7be5bb01064001dc8d8a1110
YellingOilbird <@guacharo.w3@yahoo.com>
2022-11-15 16:57:05 +0400

    up

--- contracts/tenk/src/views.rs
+++ contracts/tenk/src/views.rs
@@ -18,21 +18,27 @@ impl Contract {
     }
 
     /// Cost of NFT + fees for linkdrop
-    pub fn cost_of_linkdrop(&self, minter: &AccountId) -> U128 {
-        (self.full_link_price(minter) + self.total_cost(1, minter).0 + self.token_storage_cost().0)
+    pub fn cost_of_linkdrop(&self, minter: &AccountId, token_id: &Option<AccountId>) -> U128 {
+        (self.full_link_price(minter) + self.total_cost(1, minter, token_id).0 + self.token_storage_cost().0)
             .into()
     }
 
-    pub fn total_cost(&self, num: u16, minter: &AccountId) -> U128 {
-        (num as Balance * self.cost_per_token(minter).0).into()
+
+    pub fn total_cost(&self, num: u16, minter: &AccountId, token_id: &Option<AccountId>) -> U128 {
+        let mut cost = self.cost_per_token(minter, num).0;
+        if token_id.is_some() {
+            let token_parameters = self.get_token_parameters(token_id);
+            cost = cost / 1000 * token_parameters.token_near / 100 * token_parameters.token_boost as u128;
+        }
+        cost.into()
     }
 
     /// Flat cost of one token
-    pub fn cost_per_token(&self, minter: &AccountId) -> U128 {
+    pub fn cost_per_token(&self, minter: &AccountId, num: u16) -> U128 {
         if self.is_owner(minter) {
             0
         } else {
-            self.price()
+            self.price(num)
         }
         .into()
     }
@@ -77,7 +83,7 @@ impl Contract {
             presale_start: self.sale.presale_start.unwrap_or(MAX_DATE),
             sale_start: self.sale.public_sale_start.unwrap_or(MAX_DATE),
             status: self.get_status(),
-            price: self.price().into(),
+            price: self.price(1).into(),
             token_final_supply: self.initial(),
         }
     }
@@ -85,7 +91,12 @@ impl Contract {
     /// Information about a current user. Whether they are VIP and how many tokens left in their allowance.
     pub fn get_user_sale_info(&self, account_id: &AccountId) -> UserSaleInfo {
         let sale_info = self.get_sale_info();
-        let remaining_allowance = self.remaining_allowance(account_id);
+        let remaining_allowance = if self.is_presale() || self.sale.allowance.is_some() {
+            self.remaining_allowance(account_id)
+        } else {
+            None
+        };       
+
         UserSaleInfo {
             sale_info,
             remaining_allowance,
@@ -96,5 +107,33 @@ impl Contract {
     /// Initial size of collection. Number left to raffle + current total supply
     pub fn initial(&self) -> u64 {
         self.raffle.len() + self.nft_total_supply().0 as u64
+    }
+
+    /// Whitelisted Fungible tokens
+    pub fn get_whitelisted_tokens(&self) -> Vec<(AccountId, TokenParametersOutput)> {
+        self
+            .fungible_tokens
+            .iter()
+            .map(|(account, token_params)| {
+                let token_params_out:TokenParametersOutput = token_params.into();
+                (account, token_params_out)
+            })
+            .collect()
+    }
+
+    /// Whitelisted Fungible tokens
+    pub fn is_token_whitelisted(&self, token_id: &AccountId) -> bool {
+        self.fungible_tokens.get(token_id).is_some()
+    }
+
+    pub fn get_token_decimals(&self, token_id: Option<AccountId>) -> u8 {
+        let token_parameters = self.get_token_parameters(&token_id);
+        token_parameters.decimals
+    }
+
+    pub fn get_one_token_in_yocto(&self, token_id: AccountId) -> u128 {
+        let decimals = self.get_token_decimals(Some(token_id));
+        let one_token:u128 = 10u128.pow(decimals.into());
+        one_token
     }
 }
